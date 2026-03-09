7b91174234dcd00dbef033555bb01a7bca0f455c
Rabeya Akter <rabeyariya251@gmail.com>
2026-03-09 14:50:23 +0600

    impleemnt the product pages

---
+++ src/components/products/Brokerage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Brokerage = () => {
+    return (
+        <div>
+            <h1>Brokerage Solutions</h1>
+            <p>We offer a range of solutions for real estate brokerages.</p>
+        </div>
+    );
+};
+
+export default Brokerage;
