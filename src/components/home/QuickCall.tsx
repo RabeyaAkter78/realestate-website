27e96825e2d7b72565503aac5fb23e9605e9eaac
Rabeya Akter <rabeyariya251@gmail.com>
2026-03-25 14:32:16 +0600

    impleemnt quick call

--- src/components/home/QuickCall.tsx
+++ src/components/home/QuickCall.tsx
@@ -9,20 +9,23 @@ const QuickCall = () => {
   const [isClosed, setIsClosed] = useState(false);
 
   return (
-    <div 
+    <div
       className={`
-        ${isClosed 
-          ? "relative w-full" 
-          : "fixed bottom-6 left-1/2 -translate-x-1/2 z-50 max-w-2xl w-[90%]"
+        ${
+          isClosed
+            ? "relative w-full"
+            : "fixed bottom-6 left-1/2 -translate-x-1/2 z-50 max-w-2xl w-[90%]"
         } 
         transition-all duration-300 ease-in-out
       `}
     >
-      <div className={`
+      <div
+        className={`
         relative bg-[#eaf2f7] font-opensans px-4 sm:px-6 py-3 
         shadow-lg border border-gray-200 rounded-2xl
         ${isClosed ? "" : "rounded-2xl"}
-      `}>
+      `}
+      >
         {/* Close Button - only show when floating */}
         {!isClosed && (
           <button
@@ -34,15 +37,15 @@ const QuickCall = () => {
           </button>
         )}
 
-        <div className="flex flex-col sm:flex-row items-center justify-between gap-4 bg-white rounded-xl p-4 shadow-sm border border-gray-100 container mx-auto">
+        <div className="flex  items-center justify-between gap-4 bg-white rounded-xl p-4 shadow-sm border border-gray-100 container mx-auto">
           {/* Call Button */}
-          <button className="inline-flex items-center gap-2 bg-primary hover:bg-primary/80 text-white px-5 py-2.5 rounded-lg text-sm font-medium transition-colors">
-            <Phone className="w-4 h-4" />
+          <button className="inline-flex items-center md:gap-2 bg-primary hover:bg-primary/80 text-white px-2 py-1 sm:px-5 sm:py-2.5 rounded-lg text-sm font-medium transition-colors">
+            <Phone className="hidden sm:block w-4 h-4" />
             Call Us Now
           </button>
 
           {/* Logo */}
-          <div className="flex items-center gap-2">
+          <div className="hidden sm:block  items-center gap-2">
             <Image
               src={logo}
               alt="InstaSignTracker"
@@ -53,8 +56,8 @@ const QuickCall = () => {
           </div>
 
           {/* Schedule Demo Button */}
-          <button className="inline-flex items-center gap-2 bg-primary hover:bg-primary/80 text-white px-5 py-2.5 rounded-lg text-sm font-medium transition-all shadow-md">
-            <Calendar className="w-4 h-4" />
+          <button className="inline-flex items-center gap-2 bg-primary hover:bg-primary/80 text-white px-4 py-1 sm:px-5 sm:py-2.5 rounded-lg text-sm font-medium transition-all shadow-md">
+            <Calendar className="hidden sm:block w-4 h-4" />
             Schedule Demo
           </button>
         </div>
